35abcb4207a27ac8659f8613ad67608dc1d2c027
karko-s <121508276+karko-s@users.noreply.github.com>
2022-12-29 18:41:54 +0100

    Dodanie imienia i nazwiska

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
-    <div>Dawid Bieniaszewski</div>
-
-    
+      <div>Dawid Bieniaszewski</div>
+      <div>Karolina Sandomierz</div>
     </div>
   );
 }
