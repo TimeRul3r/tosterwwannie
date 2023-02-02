a86c87fcca206abf59b4e9113f671473c53b2f8f
ViPsAdimus <adamborzeda2002@wp.pl>
2023-02-02 18:53:17 +0100

    profiles

--- src/components/molecules/Profile.jsx
+++ src/components/molecules/Profile.jsx
@@ -1,5 +1,13 @@
 import { Name } from "../atoms/Name";
+import { Badges } from "../atoms/Badges";
+
 export const Profile = ({name}) => { console.log (name)
     
     return <div><Name name = {name}/></div>;
+  }
+
+  
+export const Profile2 = ({name}) => { console.log (name)
+    
+    return <div><Badges name = {name}/></div>;
   }
