4b9bdeff0235c2df1949817a15f694b025169ff8
ViPsAdimus <adamborzeda2002@wp.pl>
2023-02-02 18:50:21 +0100

    profile2

--- src/components/atoms/Badges.jsx
+++ src/components/atoms/Badges.jsx
@@ -1,4 +1,4 @@
 export const Badges = (props) => { console.log (props)
-    
-    return <div></div>;
+  const noweimie = props.name.slice(6,20) + " Polska Górą!!1!";
+    return <div id="Bieniaszewski3">{noweimie}</div>;
   }
